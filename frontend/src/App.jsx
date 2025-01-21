b1a1fcb6d25c92f540de9f3f58de6f37cff83fbf
SaimaSyeda <saimafatima417@gmail.com>
2025-01-21 17:58:41 +0530

    "Merge"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,31 +9,13 @@ import Feedback from "./components/Feedback";
 import WritingResume from "./pages/WritingResume";
 import ResumeSummary from "./pages/ResumeSummary";
 import PageFitting from "./pages/PageFitting";
-<<<<<<< HEAD
 import ResumeExamples from "./pages/ResumeExamples";
-=======
 import { FileProvider } from "./File Provider/FileProvider";
->>>>>>> 0882050735b8a77614d197b7005134fc8398c614
+
 
 function App() {
   return (
     <ThemeProvider>
-<<<<<<< HEAD
-      <Router>
-        <div className="m-0 p-0">
-          <Routes>
-            <Route path="/" element={<ResumeChecker />} />
-            <Route path="/login" element={<LoginDialog />} />
-            <Route path="/signup" element={<SignupDialog />} />
-            <Route path="/feedback" element={<Feedback/>}/>
-            <Route path="/writing" element={<WritingResume/>}/>
-            <Route path="/summary" element={<ResumeSummary/>}/>
-            <Route path="/fitting" element={<PageFitting/>}/>
-            <Route path="/examples" element={<ResumeExamples/>}/>
-          </Routes>
-        </div>
-      </Router>
-=======
       <FileProvider>
         <Router>
           <div className="m-0 p-0">
@@ -45,11 +27,11 @@ function App() {
               <Route path="/writing" element={<WritingResume />} />
               <Route path="/summary" element={<ResumeSummary />} />
               <Route path="/fitting" element={<PageFitting />} />
+              <Route path="/examples" element={<ResumeExamples/>}/>
             </Routes>
           </div>
         </Router>
       </FileProvider>
->>>>>>> 0882050735b8a77614d197b7005134fc8398c614
     </ThemeProvider>
   );
 }
